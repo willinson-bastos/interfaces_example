c5122c594f2c926df2da4d3af5f599e9a61b2fda
Willinson Bastos <willinson.bastos@gmail.com>
2021-08-24 16:55:24 -0300

    teste 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		.
+		Scanner scan = new Scanner(System.in);
+		
 	}
 
 }
